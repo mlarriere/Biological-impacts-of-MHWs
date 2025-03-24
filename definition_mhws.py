ad9d9711948659265a8adf1bdce3d30a7067f457
mlarriere <mlarriere@ethz.ch>
2025-03-24 09:57:44 +0100

    Average duration and sectors - to clean

--- definition_mhws.py
+++ definition_mhws.py
@@ -3,7 +3,7 @@
 """
 Created on Mon 17 Feb 09:32:05 2025
 
-Detect MHW in ROMS-SO using absolute threshold
+Calculate MHW  durations
 
 @author: Marguerite Larriere (mlarriere)
 """
@@ -36,6 +36,7 @@ z_rho = np.load('/home/jwongmeng/work/ROMS/scripts/coords/z_rho.npy')
 path_mhw = '/nfs/meso/work/jwongmeng/ROMS/model_runs/hindcast_2/output/avg/corrected/eta_chunk/' # drift and bias corrected temperature files
 joel_path ='/home/jwongmeng/work/ROMS/scripts/mhw_krill/' #codes joel
 output_path_clim = '/nfs/sea/work/mlarriere/mhw_krill_SO/clim30yrs/'
+output_path = '/nfs/sea/work/mlarriere/mhw_krill_SO/fixed_baseline30yrs/'
 
 # Sizes and dimensions
 years = range(1980, 2020)
@@ -82,7 +83,6 @@ def detect_absolute_mhw(ieta):
     # Dataset initialization
     nyears, ndays, nxi = ds_original_surf.shape
     mhw_rel_threshold = np.full((nyears, ndays, nxi), np.nan, dtype=bool)
-
 
     # Deal with NANs values
     ds_original_surf.values[np.isnan(ds_original_surf.values)] = 0 # Set to 0 so that det = False at nans
@@ -419,20 +419,20 @@ if not os.path.exists(output_file):
     ds_mhw_duration_stacked.to_netcdf(output_file, mode='w')
 
 
-# %% Visualisation 
-# read data
+# %% Visualisation of combination of events 
+# Read data
 ds_mhw_duration_stacked = xr.open_dataset(os.path.join(output_path, f"mhw_durations_extended_all_eta.nc"))
 
-year_to_plot = slice(20,24) #last idx excluded
+year_to_plot = slice(37, 39) #last idx excluded
 eta_rho_to_plot = 200 
 xi_rho_to_plot = 1000  
 
 # Slicing the dataset - ~2min of computing
 # Before combining events 
-# mhw_duration_before_to_plot = ds_mhw_duration_stacked_before.isel(years=year_to_plot, eta_rho=eta_rho_to_plot, xi_rho=xi_rho_to_plot)#dtype('int64')
-# mhw_duration_before_to_plot = mhw_duration_before_to_plot.stack(time=('years', 'days')).mhw_duration 
-# non_mhw_duration_before_to_plot = ds_non_mhw_duration_stacked_before.isel(years=year_to_plot, eta_rho=eta_rho_to_plot, xi_rho=xi_rho_to_plot) #dtype('int64')
-# non_mhw_duration_before_to_plot = non_mhw_duration_before_to_plot.stack(time=('years', 'days')).non_mhw_duration 
+mhw_duration_before_to_plot = ds_mhw_duration_stacked_before.isel(years=year_to_plot, eta_rho=eta_rho_to_plot, xi_rho=xi_rho_to_plot)#dtype('int64')
+mhw_duration_before_to_plot = mhw_duration_before_to_plot.stack(time=('years', 'days')).mhw_duration 
+non_mhw_duration_before_to_plot = ds_non_mhw_duration_stacked_before.isel(years=year_to_plot, eta_rho=eta_rho_to_plot, xi_rho=xi_rho_to_plot) #dtype('int64')
+non_mhw_duration_before_to_plot = non_mhw_duration_before_to_plot.stack(time=('years', 'days')).non_mhw_duration 
 
 # After 
 mhw_event_extended_to_plot = ds_mhw_duration_stacked.isel(years=year_to_plot, eta_rho=eta_rho_to_plot, xi_rho=xi_rho_to_plot)#dtype('int64')
@@ -444,51 +444,200 @@ lat = mhw_event_extended_to_plot.lat_rho.values.item()
 fig, ax = plt.subplots(figsize=(15, 5))
 ax.set_clip_on(False)
 
-# ax.plot(non_mhw_duration_before_to_plot, label="Non-MHW", color='#A2B36B',linestyle=":")
-# ax.plot(mhw_duration_before_to_plot, label="MHW", color='#DC6D04', linestyle="--")
+ax.plot(non_mhw_duration_before_to_plot, label="Non-MHW", color='#A2B36B',linestyle=":")
+ax.plot(mhw_duration_before_to_plot, label="MHW", color='#DC6D04', linestyle="--")
 ax.plot(mhw_event_extended_to_plot, label="Extended MHW", color='#780000', linestyle="-")
 
 ax.set_title(f'Detection of events (surface) \nLocation: ({round(lat)}°S, {round(lon)}°E)')
 ax.set_xlabel('Days')
 ax.set_ylabel('Duration (days)')
+# ax.set_ylim(-1,10)
+# ax.set_xlim(135,165)
 
 # Add the year labels below the x-axis
-# ax.annotate('', xy=(365, -65), xytext=(0, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
-# ax.annotate('', xy=(365*2, -65), xytext=(365, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
-# ax.annotate('', xy=(365*3, -65), xytext=(365*2, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
+ax.annotate('', xy=(365, -65), xytext=(0, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
+ax.annotate('', xy=(365*2, -65), xytext=(365, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
+ax.annotate('', xy=(365*3, -65), xytext=(365*2, -65), arrowprops=dict(arrowstyle="<->", color='black', lw=1.5), annotation_clip=False)
 
-# ax.text(182, -85, f'{1980 + year_to_plot.start}', ha='center', va='center')
-# ax.text(365*i + 182, -85, f'{1980 + year_to_plot.stop-i}', ha='center', va='center')
-# ax.text(365*2 + 182, -85, f'{1980 + year_to_plot.stop-1}', ha='center', va='center')
+ax.text(182, -80, f'{1980 + year_to_plot.start}', ha='center', va='center')
+ax.text(365 + 182, -80, f'{1980 + year_to_plot.start+1}', ha='center', va='center')
+ax.text(365*2 + 182, -80, f'{1980 + year_to_plot.start+2}', ha='center', va='center')
 
 ax.legend()
 plt.tight_layout()
 plt.show()
 
+# %% --- Sectors computation
+ds_mhw_duration_stacked["lon_rho"] = ds_mhw_duration_stacked["lon_rho"] % 360 # Ensure longitude is wrapped to 0-360° range
+print(ds_mhw_duration_stacked.lon_rho.values)
+print(np.isnan(ds_mhw_duration_stacked.lon_rho).sum())
+
+spatial_domain_atl = (ds_mhw_duration_stacked.lon_rho >= 290) | (ds_mhw_duration_stacked.lon_rho < 20) #Atlantic sector: From 290°E to 20°E -OK
+mask_atl = xr.DataArray(spatial_domain_atl, dims=["eta_rho", "xi_rho"]) #shape: (434, 1442)
+spatial_domain_pac = (ds_mhw_duration_stacked.lon_rho >= 150) & (ds_mhw_duration_stacked.lon_rho < 290) #Pacific sector: From 150°E to 290°E -OK
+mask_pac = xr.DataArray(spatial_domain_pac, dims=["eta_rho", "xi_rho"])
+mask_indian = ~(mask_atl | mask_pac)
 
 
-# %% Time series - spatial avg
-# Spatial average
-# mhw_duration_before_to_plot = ds_mhw_duration_stacked_before.mean(dim=['eta_rho', 'xi_rho'])#dtype('int64')
-# mhw_duration_before_to_plot = mhw_duration_before_to_plot.stack(time=('years', 'days')).mhw_duration 
-# non_mhw_duration_before_to_plot = ds_non_mhw_duration_stacked_before.mean(dim=['eta_rho', 'xi_rho'])
-# non_mhw_duration_before_to_plot = non_mhw_duration_before_to_plot.stack(time=('years', 'days')).non_mhw_duration 
-mhw_event_extended_to_plot = ds_mhw_duration_stacked.mean(dim=['eta_rho', 'xi_rho'])
-mhw_event_extended_to_plot = mhw_event_extended_to_plot.stack(time=('years', 'days')).mhw_duration 
+def apply_spatial_mask(yr, ds, variable, mask_da, sector):
 
-# Avg over time - 1 value per year
-mhw_event_avg_to_plot =  ds_mhw_duration_stacked.mean(dim=['days','eta_rho', 'xi_rho']).mhw_duration 
+    # Read only 1 year
+    ds_yr = ds.isel(years=yr)
 
+    # Mask
+    masked_data = ds_yr[variable].where(mask_da) #, drop=True) 
+    # print(masked_data.lon_rho.values)
+
+    # Reformating - new dataset
+    data_spat_filtered = xr.Dataset(
+        data_vars= dict(duration_sector = (["days", "eta_rho", "xi_rho"], masked_data.data)),
+        coords=dict(
+            lon_rho=(["eta_rho", "xi_rho"], masked_data.lon_rho.values), 
+            lat_rho=(["eta_rho", "xi_rho"], masked_data.lat_rho.values)
+            ),
+        attrs = dict(description=f'MHW duration in {sector} sector')
+        ) 
+
+    return data_spat_filtered
+
+# Spatial mask parallelization on years - computing time ~1min30  each
+spatial_mask_atl = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_mhw_duration_stacked, 'mhw_duration', mask_atl, 'atlantic') for yr in range(0, nyears))
+spatial_mask_pac = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_mhw_duration_stacked, 'mhw_duration', mask_pac, 'pacific') for yr in range(0, nyears)) 
+spatial_mask_ind = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_mhw_duration_stacked, 'mhw_duration', mask_indian, 'indian') for yr in range(0, nyears))
+
+# Put back to original dimensions
+spatial_mask_atl_all = xr.concat(spatial_mask_atl, dim='years') #~20s
+spatial_mask_pac_all = xr.concat(spatial_mask_pac, dim='years') #~20s
+spatial_mask_ind_all = xr.concat(spatial_mask_ind, dim='years') #~7min
+
+# Save output 
+output_file1 = os.path.join(output_path, f"mhw_durations_atlantic.nc")
+output_file2= os.path.join(output_path, f"mhw_durations_pacific.nc")
+output_file3 = os.path.join(output_path, f"mhw_durations_indian.nc")
+if not os.path.exists(output_file1):
+    spatial_mask_atl_all.to_netcdf(output_file1, mode='w')
+if not os.path.exists(output_file2):
+    spatial_mask_pac_all.to_netcdf(output_file2, mode='w')
+if not os.path.exists(output_file3):
+    spatial_mask_ind_all.to_netcdf(output_file3, mode='w')
+
+
+# ---------- Plot ----------
+plt.figure(figsize=(10, 10))
+ax = plt.axes(projection=ccrs.Orthographic(central_latitude=-90, central_longitude=0))
+ax.set_extent([-180, 180, -90, -60], crs=ccrs.PlateCarree())
+
+# Circular boundary
+theta = np.linspace(0, 2 * np.pi, 100)
+center, radius = [0.5, 0.5], 0.5
+verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+circle = mpath.Path(verts * radius + center)
+ax.set_boundary(circle, transform=ax.transAxes)
+
+# Plot Atlantic sector
+spatial_mask_atl_all.isel(years=0, days=98).duration_sector.plot.pcolormesh(
+    ax=ax, transform=ccrs.PlateCarree(),
+    x="lon_rho", y="lat_rho",
+    add_colorbar=False, cmap='Reds')
+
+# Plot Pacific sector
+spatial_mask_pac_all.isel(years=0, days=98).duration_sector.plot.pcolormesh(
+    ax=ax, transform=ccrs.PlateCarree(),
+    x="lon_rho", y="lat_rho",
+    add_colorbar=False, cmap='Blues')
+
+# Plot Indian sector
+spatial_mask_ind_all.isel(years=0, days=98).duration_sector.plot.pcolormesh(
+    ax=ax, transform=ccrs.PlateCarree(),
+    x="lon_rho", y="lat_rho",
+    add_colorbar=False, cmap='Greens')
+
+
+ax.coastlines(color='black', linewidth=1.5, zorder=1)
+ax.add_feature(cfeature.LAND, zorder=2,  facecolor='#F6F6F3')
+ax.set_facecolor('lightgrey')
+
+# Atlantic-Pacific boundary (near Drake Passage)
+ax.plot([-70, -70], [-90, -60], transform=ccrs.PlateCarree(), color='darkred', linestyle='--', linewidth=3) #Atlantic sector
+
+# Pacific-Indian boundary
+ax.plot([150, 150], [-90, -60], transform=ccrs.PlateCarree(), color='darkred', linestyle='--', linewidth=3) #Pacific sector
+
+# Indian-Atlantic boundary
+ax.plot([20, 20], [-90, -60], transform=ccrs.PlateCarree(), color='darkred', linestyle='--', linewidth=3) #Indian sector
+ax.gridlines(draw_labels=True)
+
+plt.show()
+
+
+
+
+# %% Yearly averages (from code: avg_mhw_durations.py)
+ds_avg_duration = xr.open_dataset(os.path.join(output_path, f"mhw_avg_duration_yearly.nc"))
+ds_avg_duration_SO = ds_avg_duration.avg_dur.mean(dim=['eta_rho', 'xi_rho'])
+
+# --- Sectors 
+spatial_domain_atl = (ds_avg_duration.lon_rho >= 290) | (ds_mhw_duration_stacked.lon_rho < 20) #Atlantic sector: From 290°E to 20°E -OK
+mask_atl = xr.DataArray(spatial_domain_atl, dims=["eta_rho", "xi_rho"]) #shape: (434, 1442)
+spatial_domain_pac = (ds_avg_duration.lon_rho >= 150) & (ds_mhw_duration_stacked.lon_rho < 290) #Pacific sector: From 150°E to 290°E -OK
+mask_pac = xr.DataArray(spatial_domain_pac, dims=["eta_rho", "xi_rho"])
+mask_indian = ~(mask_atl | mask_pac)
+
+def apply_spatial_mask(yr, ds, variable, mask_da, sector):
+
+    # Read only 1 year
+    ds_yr = ds.isel(years=yr)
+
+    # Mask
+    masked_data = ds_yr[variable].where(mask_da) #, drop=True) 
+    # print(masked_data.lon_rho.values)
+
+    # Reformating - new dataset
+    data_spat_filtered = xr.Dataset(
+        data_vars= dict(duration_sector = (["eta_rho", "xi_rho"], masked_data.data)),
+        coords=dict(
+            lon_rho=(["eta_rho", "xi_rho"], masked_data.lon_rho.values), 
+            lat_rho=(["eta_rho", "xi_rho"], masked_data.lat_rho.values)
+            ),
+        attrs = dict(description=f'MHW duration in {sector} sector')
+        ) 
+
+    return data_spat_filtered
+
+avg_duration_mask_atl = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_avg_duration, 'avg_dur', mask_atl, 'atlantic') for yr in range(0, nyears))
+avg_duration_mask_pac = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_avg_duration, 'avg_dur', mask_pac, 'pacific') for yr in range(0, nyears))
+avg_duration_mask_ind = Parallel(n_jobs=30)(delayed(apply_spatial_mask)(yr, ds_avg_duration, 'avg_dur', mask_indian, 'indian') for yr in range(0, nyears))
+
+# Put back to original dimensions
+spatial_mask_atl_all = xr.concat(avg_duration_mask_atl, dim='years') #~20s
+spatial_mask_pac_all = xr.concat(avg_duration_mask_pac, dim='years') #~20s
+spatial_mask_ind_all = xr.concat(avg_duration_mask_ind, dim='years') #~7min
+
+ds_avg_duration_atl = spatial_mask_atl_all.duration_sector.mean(dim=['eta_rho', 'xi_rho'])
+ds_avg_duration_pac = spatial_mask_pac_all.duration_sector.mean(dim=['eta_rho', 'xi_rho'])
+ds_avg_duration_ind = spatial_mask_ind_all.duration_sector.mean(dim=['eta_rho', 'xi_rho'])
+
+# --- PLOT
+# Adding trend lines
+m, b = np.polyfit(ds_avg_duration_SO.years,ds_avg_duration_SO.values,  1)
 
 fig, ax = plt.subplots(figsize=(15, 5))
 ax.set_clip_on(False)
-# ax.plot(non_mhw_duration_before_to_plot, label="Non-MHW", color='#A2B36B',linestyle=":")
-# ax.plot(mhw_duration_before_to_plot, label="MHW", color='#DC6D04', linestyle="--")
-ax.plot(mhw_event_avg_to_plot, label="Extended MHW", color='#780000', linestyle="-")
+ax.plot(ds_avg_duration_SO, label="Southern Ocean", color='black', linewidth = 1.5, linestyle="-")
+ax.plot(ds_avg_duration_SO.years, m*ds_avg_duration_SO.years + b, color='black',linestyle="--" )
+ax.plot(ds_avg_duration_atl, label="Atlantic", color='#778B04', linewidth = 1, linestyle="-")
+ax.plot(ds_avg_duration_pac, label="Pacific", color='#BF3100', linewidth = 1, linestyle="-")
+ax.plot(ds_avg_duration_ind, label="Indian", color='#E09F3E', linewidth = 1, linestyle="-")
 ax.set_title(f'Spatially averaged MHW duration (surface)')
-ax.set_xlabel('Days')
+ax.set_xlabel('Years')
 ax.set_ylabel('Duration (days)')
 ax.legend()
+years = list(range(1980, 2020, 5)) + [2019]  # Ensure 2019 is included
+years.sort()  # Keep order
+ax.set_xticks(np.array(years) - 1980)  # Shift to match index if necessary
+ax.set_xticklabels(years)
+ax.set_xlim(0, nyears-1)
+
 plt.tight_layout()
 plt.show()
 
@@ -529,6 +678,7 @@ pcolormesh = mhw_plot_data.mhw_duration.plot.pcolormesh(
     add_colorbar=False, 
     # vmin=0, vmax=30,
     cmap='viridis')
+
 
 # -------- Plot point --------
 # sc = ax.scatter(lon, lat, c=[value], cmap='coolwarm', vmin=-5, vmax=5,
