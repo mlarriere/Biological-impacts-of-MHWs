536566e70fe9ed381ec21bb7191fb3444cab7447
mlarriere <mlarriere@ethz.ch>
2025-03-26 10:56:36 +0100

    new folder

---
+++ Marine_HeatWaves/mhw_frequency.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon 26 March 10:34:17 2025
+
+Frequency of the 4 different events: SST > (relative + absolute thresholds)
+
+@author: Marguerite Larriere (mlarriere)
+"""
+
+# %% --------------------------------PACKAGES------------------------------------
+import os
+import xarray as xr
+import numpy as np
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+
+from joblib import Parallel, delayed
+
+
+# %% -------------------------------- SETTINGS --------------------------------
+# Set working directory
+working_dir = "/home/mlarriere/Projects/biological_impacts_MHWs/Biological-impacts-of-MHWs/"
+os.chdir(working_dir)
+print("Working directory set to:", os.getcwd())
+
+# Directories
+ds_roms = xr.open_dataset('/nfs/meso/work/jwongmeng/ROMS/model_runs/hindcast_2/output/avg/SO_d025_avg_daily_1979.nc')
+z_rho = np.load('/home/jwongmeng/work/ROMS/scripts/coords/z_rho.npy')
+
+path_temp = '/nfs/meso/work/jwongmeng/ROMS/model_runs/hindcast_2/output/avg/corrected/eta_chunk/' # drift and bias corrected temperature files
+path_clim = '/nfs/sea/work/mlarriere/mhw_krill_SO/clim30yrs/'
+path_det = '/nfs/sea/work/mlarriere/mhw_krill_SO/fixed_baseline30yrs/'
+
+# Sizes and dimensions
+years = range(1980, 2020)
+nyears = np.size(years)
+months = range(1, 13)
+days = range(0, 365)
+ndays = np.size(days)
+nz = 35  # depths levels
+neta = 434 # lat
+nxi = 1442  # lon
+
+# -- Define Thresholds
+absolute_thresholds = [1, 2, 3, 4] # Fixed absolute threshold
